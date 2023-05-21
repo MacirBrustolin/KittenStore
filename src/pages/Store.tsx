f448f2ac341ef1a5fe282fd70993c742c4267854
Macir Brustolin <mj.brustolin@hotmail.com>
2023-05-21 20:31:04 -0300

    commit

--- src/pages/Store.tsx
+++ src/pages/Store.tsx
@@ -1,5 +1,15 @@
+import { Col, Row } from "react-bootstrap"
+import storeItems from "../data/items.json"
+import { StoreItem } from "../components/StoreItem"
 export function Store() {
     return (
-      <div>Store</div>
+      <>
+        <h1>Store</h1>
+        <Row xs={1} md={2} xl={3} className="g-3">
+          {storeItems.map(item => (
+            <Col key={item.id}><StoreItem {...item}/></Col>
+          ))}
+        </Row>
+      </>
     )
   }
